a3b1d4124614d4350da1fffc739800aaba10a998
Ludovic RAVINET <ludovic.ravinet@gmail.com>
2021-05-12 03:20:02 +0200

    filter hiking

--- src/Model/HikingManager.php
+++ src/Model/HikingManager.php
@@ -5,4 +5,12 @@ namespace App\Model;
 class HikingManager extends AbstractManager
 {
     public const TABLE = 'mark';
+
+
+    public function selectAllFilter(int $valeur): array
+    {
+        $query = 'SELECT * FROM mark WHERE filter = ' . $valeur . ';';
+        $list = $this->pdo->query($query)->fetchAll();
+        return $list;
+    }
 }
